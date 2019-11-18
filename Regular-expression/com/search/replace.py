559436c10329a69effbb22629feaf863be82990d
kailgege <1491685086@qq.com>
2019-11-18 21:14:33 +0800

    2019-11-18-2

---
+++ Regular-expression/com/search/replace.py
@@ -0,0 +1,16 @@
+'''
+Created on 2019年11月14日
+
+@author: 13476
+'''
+import re
+from docx import Document
+
+string1=input("需要替换的字符串：")
+string2=input("替换的字符串：")
+d=Document('test/AA.docx')
+for dparagraph in d.paragraphs:
+    #dparagraph.text= dparagraph.text.replace('是','ABC')
+    dparagraph.text=re.sub(string1,string2,dparagraph.text)
+d.save('test/AA_out.docx')
+print("替换成功")
