d711a976023f57336845607d67736f2d55e8e783
kailgege <1491685086@qq.com>
2019-12-01 21:03:48 +0800

    2019-12-1-3

--- Regular-expression/com/search/search2.py
+++ Regular-expression/com/search/search2.py
@@ -1,15 +1,22 @@
 '''
-Created on 2019年11月14日
+Created on 2019年11月18日
 
 @author: 13476
 '''
-'''
-import os
-print(os.path.abspath('TestDir/dir2/doc22.docx'))
-'''
-import docx
+import re
 from docx import Document
-path = "C:\\Users\\13476\\Desktop\\word.docx"
-document = Document(path)
-for paragraph in document.paragraphs:
-    print(paragraph.text)
+
+f=Document('test/AA.docx')
+#string=input("需要查找的字符串：")
+
+re_f=re.compile(u"[\u4e00-\u9fa5]+。") 
+#re_f=re.compile(r'(.+|\s?)(Python.+[\u4E00-\u9FA5]+。)') 
+re_f1=re.compile(u"Python.+[\u4E00-\u9FA5]+。")
+re_f2=re.compile(r'\W(Python.+[\u4E00-\u9FA5]+。)') 
+for fparagraph in f.paragraphs:
+    findall_f=re_f1.findall(fparagraph.text)
+    findall_f1=re_f1.findall(fparagraph.text)
+    findall_f2=re_f2.findall(fparagraph.text)
+    print(findall_f)
+    #print(findall_f1)
+    #print(findall_f2)
