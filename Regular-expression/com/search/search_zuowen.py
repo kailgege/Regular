d5e2e8aaf4098e1559d0daa38ed64d3a805affd4
kailgege <1491685086@qq.com>
2020-01-02 21:45:48 +0800

    2010-1-2-8

--- Regular-expression/com/search/search_zuowen.py
+++ Regular-expression/com/search/search_zuowen.py
@@ -8,17 +8,18 @@ from docx import Document
 
 f=Document('test/zuowen.docx')
 
-#u"[\u3000-\u303f\ufb00-\ufffd]+"  "unicode 标点符号"   :\uff1a   ,\uff0c   ;\uff1b 
-#re_f=re.compile(u'[\u4e00-\u9fa5|\uff0c|\uff1b|\uff1a]*十八岁[\u4e00-\u9fa5|\uff0c|\uff1b|\uff1a]*') 
-
-re_f=re.compile(u'[\u4e00-\u9fa5|\u3000-\u303f|\ufb00-\ufffd]*十八岁[\u4e00-\u9fa5||\u3000-\u303f|\ufb00-\ufffd]*') 
+string=input("需要替换的字符串：")
+'''
+unicode 标点符号     u"[\u3000-\u303f\ufb00-\ufffd]+"  unicode 中文字符  u'[\u4e00-\u9fa5]+'
+只匹配中文：re_f=re.compile(u'[\u4e00-\u9fa5|\u3000-\u303f|\ufb00-\ufffd]*十八岁[\u4e00-\u9fa5||\u3000-\u303f|\ufb00-\ufffd]*') 
+'''
+#可匹配中文中混有英文
+re_f=re.compile(u'[\w\W\u4e00-\u9fa5|\u3000-\u303f|\ufb00-\ufffd]*'+string+'[\w\W\u4e00-\u9fa5||\u3000-\u303f|\ufb00-\ufffd]*') 
 re_p=re.compile(r'。|？|！|……')
 for fparagraph in f.paragraphs:
-    #p=fparagraph.text.split(r"。")
     p=re.split(re_p,fparagraph.text)
-    #print(p)
+    #print(p)            #输出分割后的句子
     for item in p:
-        #加if判断减去空的结果
         findall_f=re_f.findall(item)  
         for find_f in findall_f:
             print(find_f+'。')
