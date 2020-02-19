60c22e632f8dca8c9914c5b6888937e477627307
kailgege <1491685086@qq.com>
2020-02-19 20:04:05 +0800

    2020-2-19

---
+++ Regular-expression/com/search/Name.py
@@ -0,0 +1,23 @@
+'''
+Created on 2020年2月10日
+
+@author: 13476
+'''
+import re
+from docx import Document
+
+f=Document('test/zuowenName.docx')
+'''
+var username = /^[\u4E00-\u9FA5A-Za-z]+$/; 
+or /^([a-zA-Z0-9\u4e00-\u9fa5\·]{1,10})$/   
+ var reg = /^[\u4E00-\u9FA5\uf900-\ufa2d·s]{2,20}$/;//验证姓名正则 
+ '''
+re_f=re.compile(r"/^[\u4E00-\u9FA5\uf900-\ufa2d·s]{2,20}$/")
+re_p=re.compile(r'\!|。|？|！|……')
+for fparagraph in f.paragraphs:
+    p=re.split(re_p,fparagraph.text)
+    #print(p)            #输出分割后的句子
+    for item in p:
+        findall_f=re_f.findall(item)  
+        for find_f in findall_f:
+            print(find_f)
