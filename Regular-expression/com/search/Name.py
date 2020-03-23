bf3483a7d5757ea4d37179e77c6236c34a78a482
kailgege <1491685086@qq.com>
2020-03-23 21:18:47 +0800

    20-3-23

--- Regular-expression/com/search/Name.py
+++ Regular-expression/com/search/Name.py
@@ -1,4 +1,4 @@
-'''
+﻿'''
 Created on 2020年2月10日
 
 @author: 13476
@@ -7,15 +7,10 @@ import re
 from docx import Document
 
 f=Document('test/zuowenName.docx')
-'''
-var username = /^[\u4E00-\u9FA5A-Za-z]+$/; 
-or /^([a-zA-Z0-9\u4e00-\u9fa5\·]{1,10})$/   
- var reg = /^[\u4E00-\u9FA5\uf900-\ufa2d·s]{2,20}$/;//验证姓名正则 
- 
- 
-^([\u4e00-\u9fa5]+|([a-zA-Z]+\s?)+)$ 
- '''
-re_f=re.compile(r"^[\u4E00-\u9FA5\uf900-\ufa2d·s]{2,20}$")
+
+#re_f=re.compile(u'^[\u4E00-\u9FA5\uf900-\ufa2d·s]{2,20}$')
+#re_f=re.compile(u'^([a-zA-Z+\s]{2,20})$')
+re_f=re.compile(u'^([\u4E00-\u9FA5·]{2,20})$')
 re_p=re.compile(r'\!|。|？|！|……')
 for fparagraph in f.paragraphs:
     p=re.split(re_p,fparagraph.text)
